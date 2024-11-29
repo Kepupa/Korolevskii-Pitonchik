618cf0654d733b3fe0760bf8d25a40cc95fbf872
Kepupa <2Sa1kle2@mail.ru>
2024-11-29 21:58:51 +0300

    update

--- main.py
+++ main.py
@@ -7,30 +7,24 @@ class ClickerApp(QWidget):
         self.init_ui()
 
     def init_ui(self):
-        # Инициализируем количество кликов
         self.click_count = 0
 
-        # Создаем элементы интерфейса
         self.label = QLabel(f"Clicks: {self.click_count}", self)
         self.button = QPushButton("Click me", self)
         
-        # Соединяем сигнал от кнопки с функцией увеличения кликов
         self.button.clicked.connect(self.increment_clicks)
-        
-        # Создаем макет
+
         layout = QVBoxLayout()
         layout.addWidget(self.label)
         layout.addWidget(self.button)
         
         self.setLayout(layout)
         
-        # Настроим окно
         self.setWindowTitle('simple clicker')
         self.setGeometry(100, 100, 300, 200)
         self.show()
 
     def increment_clicks(self):
-        # Увеличиваем количество кликов и обновляем текст на лейбле
         self.click_count += 1
         self.label.setText(f"Clicks: {self.click_count}")
 
